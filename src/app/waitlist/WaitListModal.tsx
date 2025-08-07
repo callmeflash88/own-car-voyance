10ad14e9b5ffaef74857064729f073f78b486777
callmeflash88 <Volodymyr_Kozak@comebackmail.com>
2025-08-07 21:19:39 +0300

    change ui

---
+++ src/app/waitlist/WaitListModal.tsx
@@ -0,0 +1,43 @@
+// components/WaitlistModal.tsx
+"use client";
+
+import { X } from "lucide-react";
+import { Button } from "@/shared/ui";
+import { useEffect } from "react";
+
+export default function WaitlistModal({ onClose }: { onClose: () => void }) {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, [onClose]);
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+      <div className="relative bg-white rounded-xl p-8 max-w-[800px] max-h-[350px] w-full text-center shadow-lg">
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
+        >
+          <X size={20} />
+        </button>
+        <div className="my-14">
+          <h2 className=" font-normal text-[60px] leading-[100%] tracking-[0%] uppercase">
+            COMING SOON
+          </h2>
+          <p className="font-inter font-semibold text-[16px] leading-[1.4] tracking-[0] text-center text-[#00000099] mt-7">
+            We’re working hard to launch soon. <br /> Join the waitlist to be
+            the first to know when we go live
+          </p>
+          <div className="mt-8 flex justify-center">
+            <Button variant="primary" size="lg" onClick={onClose}>
+              Join The Waitlist
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import Image from "next/image";
-import bgWaitlist from "../../../public/assets/backgrounds/bgWaitlistHeader.jpg";
+import logo from "@/shared/assets/waitList/logo.svg";
 import waitListLogo from "../../../public/assets/icons/waitlistLogo.svg";
-import waitListImage from "../../../public/assets/images/waitList3.svg";
-import waitListMobile from "../../../public/assets/images/waitListMobile.svg";
+import waitListPhone from "@/shared/assets/waitList/waitListPhone.svg";
+import waitListPhoneMobile from "@/shared/assets/waitList/waitListPhoneMobile.png";
 import { Instagram, Phone } from "lucide-react";
 import { Button, Input } from "@/shared/ui";
 import { FaTiktok } from "react-icons/fa";
@@ -14,6 +14,7 @@ import { useState } from "react";
 import { cn } from "@/shared/lib/utils";
 import ContactSwitcher from "./ContactSwitcher";
 import { PhoneInput } from "./PhoneInput";
+import WaitlistModal from "./WaitListModal";
 
 export default function WaitlistPage() {
   const {
@@ -30,167 +31,192 @@ export default function WaitlistPage() {
   } = useWaitlist();
   const [method, setMethod] = useState<"phone" | "email">("phone");
   const [isPhoneConsentChecked, setIsPhoneConsentChecked] = useState(false);
-
   const [isMailForm, setIsMailForm] = useState(false);
+  const [isFormShow, setIsFormShow] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className="relative overflow-hidden">
-      <header
-        className="h-[88px] w-full bg-cover bg-center px-[160px] flex justify-center lg:justify-between items-center z-20 relative"
-        style={{
-          backgroundImage: `url(${bgWaitlist.src})`,
-        }}
-      >
+      <header className="mt-6 h-[88px] w-full bg-cover bg-center px-10 lg:px-[160px] flex justify-between lg:justify-between items-center z-20 relative">
         <Image
-          src={waitListLogo}
+          src={logo}
           alt="logo"
-          className="max-w-none !w-[240px] lg:w-[100px]"
+          className="max-w-none !w-[80px] lg:w-[100px]"
         />
-        <div className="hidden gap-4 lg:flex">
-          <a
-            href="https://www.instagram.com/carvoyance"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-2 rounded-full bg-white inline-flex items-center justify-center"
-          >
-            <Instagram size={16} />
-          </a>
-          <a
-            href="https://www.tiktok.com/@carvoyance"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-2 rounded-full bg-white inline-flex items-center justify-center"
-          >
-            <FaTiktok />
-          </a>
-          <a
-            href="https://www.threads.com/@carvoyance"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-2 rounded-full bg-white inline-flex items-center justify-center"
-          >
-            <FaThreads />
-          </a>
-        </div>
+        <ul className="flex gap-10 justify-center items-center font-inter">
+          {["Home", "Download", "About Us", "Contact"].map((item) => (
+            <li
+              key={item}
+              onClick={() => setIsModalOpen(true)}
+              className="cursor-pointer hover:underline lg:block hidden"
+            >
+              {item}
+            </li>
+          ))}
+          <div className="flex flex-col justify-between w-6 h-4 ml-6 cursor-pointer">
+            <span className="block h-[2px] w-8 bg-[#4E17E5] rounded-full"></span>
+            <span className="block h-[2px] w-8 bg-[#4E17E5] rounded-full"></span>
+          </div>
+        </ul>
       </header>
 
-      <div className="hidden lg:block absolute right-0 top-1/2 -translate-y-1/2 w-[1200px] h-[800px] bg-[#6F76F2] rounded-full blur-[200px] opacity-20 z-0 pointer-events-none" />
+      {/* <div className="hidden lg:block absolute right-0 top-1/2 -translate-y-1/2 w-[1200px] h-[800px] bg-[#6F76F2] rounded-full blur-[200px] opacity-20 z-0 pointer-events-none" /> */}
 
       <div className="relative z-10 max-w-[100vw] overflow-hidden min-h-[80vh] mt-10 lg:mt-0 px-4 lg:px-[160px] flex flex-col xl:flex-row items-center justify-between">
-        {/* <div className="hidden lg:block absolute right-0 top-1/2 -translate-y-1/2 w-[1200px] h-[800px] bg-[#6F76F2] rounded-full blur-[200px] opacity-20 z-0 pointer-events-none" /> */}
-
         <div className="flex flex-col justify-center items-center lg:items-start lg:flex-1">
-          <h4 className="font-inter font-semibold text-2xl uppercase text-[#4E17E5]">
-            Coming soon
-          </h4>
-          <h1 className="font-inter font-semibold text-[36px] lg:text-5xl mt-3 text-center lg:text-left leading-normal lg:leading-[64px]">
-            Be the first to know
-            <br /> when CarVoyance
-            <br /> is ready!
-          </h1>
-
-          <p className="mt-3 font-inter text-base text-center lg:text-left text-[#2B2B2B]">
-            Sign up to join our waitlist and get{" "}
-            <br className="block lg:hidden" /> notified as soon as we go live!
-          </p>
+          <h2 className="font-medium text-[80px] lg:text-[140px] leading-[120%] tracking-[0%] font-inter">
+            COMING
+            <br /> <span className="text-[#4E17E5]">Fall 2025</span>
+          </h2>
 
           {/*  */}
-          <div className="w-full max-w-[500px] border-2 border-[#292929] rounded-full mt-4 flex justify-between transition-all">
-            <button
-              onClick={() => setIsMailForm(false)}
-              className={`w-full px-4 py-2 rounded-full text-sm font-medium font-inter transition-all duration-200 ${
-                !isMailForm
-                  ? "bg-[#292929] text-white"
-                  : "bg-transparent text-[#292929]"
-              }`}
-            >
-              Phone
-            </button>
-            <button
-              onClick={() => setIsMailForm(true)}
-              className={`w-full px-4 py-1 rounded-full text-sm font-medium font-inter transition-all duration-200 ${
-                isMailForm
-                  ? "bg-[#292929] text-white"
-                  : "bg-transparent text-[#292929]"
-              }`}
-            >
-              Email
-            </button>
-          </div>
-
-          <div className="flex flex-col justify-start mt-7 gap-2">
-            {isMailForm ? (
-              <Input
-                label="Stay Updated Via mail"
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="!w-[80vw]  lg:!w-[470px]"
-                autoComplete="email"
-                inputMode="email"
-              />
-            ) : (
-              <>
-                <PhoneInput
-                  label="Stay Updated Via Phone"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  className=""
-                  icon={<Phone size={16} />}
-                  iconPosition="left"
-                />
-                <label className="text-sm flex items-start gap-2 mt-2 w-[500px]">
-                  <input
-                    type="checkbox"
-                    checked={isPhoneConsentChecked}
-                    onChange={(e) => setIsPhoneConsentChecked(e.target.checked)}
-                    className="mt-1"
-                  />
-                  <span className="text-[#2B2B2B80]">
-                    By submitting your phone number, you agree to receive SMS
-                    updates from CarVoyance. Message & data rates may apply.
-                  </span>
-                </label>
-              </>
-            )}
-
-            {isCodeSent && !isVerified && (
-              <>
-                <Input
-                  label="Enter verification code"
-                  type="text"
-                  placeholder="123456"
-                  value={code}
-                  onChange={(e) => setCode(e.target.value)}
-                  className="!w-[80vw] h-[5vh] lg:!w-[400px]"
-                  inputMode="numeric"
-                />
-                <Button
-                  variant="secondary"
-                  size="md"
-                  className="px-10 w-full py-3"
-                  onClick={verifyCode}
+          {isFormShow && (
+            <div className="w-full max-w-[600px] mt-4">
+              <div className="w-full border-2 border-[#292929] rounded-full flex justify-between transition-all">
+                <button
+                  onClick={() => setIsMailForm(false)}
+                  className={`w-1/2 px-4 py-2 rounded-full text-sm font-medium font-inter transition-all duration-200 ${
+                    !isMailForm
+                      ? "bg-[#292929] text-white"
+                      : "bg-transparent text-[#292929]"
+                  }`}
                 >
-                  Verify Code
-                </Button>
-              </>
-            )}
+                  Phone
+                </button>
+                <button
+                  onClick={() => setIsMailForm(true)}
+                  className={`w-1/2 px-4 py-2 rounded-full text-sm font-medium font-inter transition-all duration-200 ${
+                    isMailForm
+                      ? "bg-[#292929] text-white"
+                      : "bg-transparent text-[#292929]"
+                  }`}
+                >
+                  Email
+                </button>
+              </div>
 
-            {!isCodeSent && (
+              <div className="flex flex-col justify-start mt-7 gap-4 w-full">
+                {isMailForm ? (
+                  <Input
+                    label="Stay Updated Via Email"
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full"
+                    autoComplete="email"
+                    inputMode="email"
+                  />
+                ) : (
+                  <>
+                    <PhoneInput
+                      label="Stay Updated Via Phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                      className="w-full"
+                      icon={<Phone size={16} />}
+                      iconPosition="left"
+                    />
+                    <label className="text-sm flex items-start gap-2 mt-2">
+                      <input
+                        type="checkbox"
+                        checked={isPhoneConsentChecked}
+                        onChange={(e) =>
+                          setIsPhoneConsentChecked(e.target.checked)
+                        }
+                        className="mt-1"
+                      />
+                      <span className="text-[#2B2B2B80]">
+                        By submitting your phone number, you agree to receive
+                        SMS updates from CarVoyance. Message & data rates may
+                        apply.
+                      </span>
+                    </label>
+                  </>
+                )}
+
+                {isCodeSent && !isVerified && (
+                  <>
+                    <Input
+                      label="Enter verification code"
+                      type="text"
+                      placeholder="123456"
+                      value={code}
+                      onChange={(e) => setCode(e.target.value)}
+                      className="w-full"
+                      inputMode="numeric"
+                    />
+                    <Button
+                      variant="secondary"
+                      size="md"
+                      className="w-full py-3"
+                      onClick={verifyCode}
+                    >
+                      Verify Code
+                    </Button>
+                  </>
+                )}
+
+                {!isCodeSent && (
+                  <Button
+                    variant="primary"
+                    size="md"
+                    className="w-full py-3"
+                    onClick={() =>
+                      handleSubmit(isMailForm, isPhoneConsentChecked)
+                    }
+                  >
+                    Submit
+                  </Button>
+                )}
+              </div>
+            </div>
+          )}
+
+          {/*  */}
+
+          <div
+            className={`w-full flex flex-col justify-center mt-5 ${
+              isFormShow ? "items-start" : "items-center"
+            }`}
+          >
+            {!isFormShow && (
               <Button
                 variant="primary"
-                size="md"
-                className="px-10 w-full py-3"
-                onClick={() => handleSubmit(isMailForm, isPhoneConsentChecked)}
+                size="lg"
+                className="py-3 px-14"
+                onClick={() => setIsFormShow(true)}
               >
-                Submit
+                Join The Waitlist
               </Button>
             )}
+
+            <p className="font-normal text-[18px] leading-[155%] tracking-[0%] font-inter mt-5">
+              One platform. Endless possibilities.
+              <br className="block lg:hidden" /> Welcome to the future of car
+              ownership
+            </p>
           </div>
         </div>
+
         <div className="relative flex justify-end items-center lg:flex-1">
           <Image
+            src={waitListPhone}
+            alt="waitlist"
+            width={600}
+            height={1000}
+            priority
+            className="object-contain hidden lg:block relative z-10"
+          />
+          <Image
+            src={waitListPhoneMobile}
+            alt="waitlist"
+            width={600}
+            height={1000}
+            priority
+            className="object-contain block lg:hidden relative z-10 mt-8"
+          />
+          {/* <Image
             src={waitListImage}
             alt="waitlist"
             width={600}
@@ -205,11 +231,11 @@ export default function WaitlistPage() {
             height={1000}
             priority
             className="object-contain block lg:hidden relative z-10"
-          />
+          /> */}
         </div>
       </div>
 
-      <footer className="relative z-10 px-4 lg:px-[160px] h-[88px] flex items-center justify-between">
+      <footer className="bg-[#5511EE] relative z-10 px-4 lg:px-[160px] h-[62px] flex items-center justify-between text-white">
         <div className="flex flex-col justify-center items-start gap-2">
           <a
             href="https://app.termly.io/policy-viewer/policy.html?policyUUID=69dca727-6b6b-487b-ab63-a498cf38780d"
@@ -234,34 +260,10 @@ export default function WaitlistPage() {
           >
             Privacy Policy
           </a>
-          <div className="flex gap-4 lg:hidden">
-            <a
-              href="https://www.instagram.com/carvoyance"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-2 rounded-full bg-white inline-flex items-center justify-center"
-            >
-              <Instagram size={16} />
-            </a>
-            <a
-              href="https://www.tiktok.com/@carvoyance"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-2 rounded-full bg-white inline-flex items-center justify-center"
-            >
-              <FaTiktok />
-            </a>
-            <a
-              href="https://www.threads.com/@carvoyance"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-2 rounded-full bg-white inline-flex items-center justify-center"
-            >
-              <FaThreads />
-            </a>
-          </div>
         </div>
       </footer>
+
+      {isModalOpen && <WaitlistModal onClose={() => setIsModalOpen(false)} />}
     </div>
   );
 }
