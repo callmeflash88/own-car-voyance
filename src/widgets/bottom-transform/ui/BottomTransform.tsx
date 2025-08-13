3bda62b302910c8f6da2a2d8c8145e96db419333
osyshyn <ceo@comebackagency.com>
2025-08-13 18:03:05 +0300

    quick ui fix

--- src/widgets/bottom-transform/ui/BottomTransform.tsx
+++ src/widgets/bottom-transform/ui/BottomTransform.tsx
@@ -5,19 +5,16 @@ import { Button } from "@/shared/ui";
 export const BottomTransform = () => {
   return (
     <section className="px-4 w-full flex justify-center items-center">
-      <div
-        className="rounded-[40px] bg-cover bg-center bg-no-repeat lg:h-[500px] w-full py-10 lg:py-32 overflow-hidden"
-        style={{ backgroundImage: "url('/assets/backgrounds/bottomBg.jpg')" }}
-      >
+      <div className="rounded-[40px] bg-cover bg-center bg-no-repeat lg:h-[500px] w-full py-10 lg:py-32 overflow-hidden bg-gradient-to-r from-[#4E17E5] from-11% to-[#2B2B2B] to-104%">
         <div className="w-full h-full flex flex-col gap-10 px-4 lg:flex-row lg:gap-0 lg:px-20 justify-between items-center">
           <div className="flex flex-col w-full xs:items-center">
             <h3 className="font-inter font-semibold text-[28px] lg:text-[56px] leading-[120%] tracking-[1%] text-[#FFFFFF]">
               Ready to Transform
               <br /> Your Car Experience?
             </h3>
-            <p className="text-[#FFFFFFB3] mt-4 text-base lg:text-lg max-w-[400px] lg:max-w-[600px]">
-              Join thousands of satisfied users who have discovered a <br />{" "}
-              better way to buy and sell vehicles.
+            <p className="text-[#FFFFFF80] mt-4 text-base lg:text-lg max-w-[400px] lg:max-w-[600px]">
+              Join thousands of satisfied users who have discovered a better way
+              to buy and sell vehicles.
             </p>
             <div className="mt-10 flex items-center justify-start gap-3 flex-wrap">
               <Button
@@ -30,7 +27,7 @@ export const BottomTransform = () => {
               <Button
                 variant="outline"
                 size="lg"
-                className="flex-1 text-white text-base"
+                className="flex-1 text-white border-white text-base"
               >
                 Learn More
               </Button>
